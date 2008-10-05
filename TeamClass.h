8a794c48a7184279ce2cba56cd12582b97a7f46c
pd <pdmail@gmx.de>
2008-10-05 10:09:36 +0000

    git-svn-id: svn://svn.renegadeprojects.com/yrpp/trunk@183 bbc44d77-a14d-0410-bbfb-905795908d03

---
+++ TeamClass.h
@@ -0,0 +1,59 @@
+#ifndef TEAM_H
+#define TEAM_H
+
+#include <TeamTypeClass.h>
+
+class HouseClass;
+class ScriptClass;
+
+class TeamClass : public AbstractClass
+{
+public:
+	//Static
+	static DynamicVectorClass<TeamClass*>* Array;
+
+	//IPersist
+	virtual HRESULT _stdcall GetClassID(CLSID* pClassID) R0;
+
+	//IPersistStream
+	virtual HRESULT _stdcall Load(IStream* pStm) R0;
+	virtual HRESULT _stdcall Save(IStream* pStm,BOOL fClearDirty) R0;
+
+	//Destructor
+	virtual ~TeamClass() RX;
+
+	//AbstractClass
+	virtual eAbstractType WhatAmI() R0;
+	virtual int Size() R0;
+
+	//Constructor
+	TeamClass(TeamTypeClass* pType)
+	{
+		PUSH_VAR32(pType);
+		THISCALL(0x6E8A90);
+	}
+
+protected:
+	TeamClass() : AbstractClass(false) {}
+	TeamClass(bool X) : AbstractClass(X){}
+
+	//===========================================================================
+	//===== Properties ==========================================================
+	//===========================================================================
+	PROPERTY(TeamTypeClass*, Type);
+	PROPERTY(ScriptClass*, CurrentScript);
+	PROPERTY(HouseClass*, Owner);
+	PROPERTY(DWORD, unknown_30);
+	PROPERTY(DWORD, unknown_34);
+	PROPERTY(DWORD, unknown_38);
+	PROPERTY(DWORD, unknown_3C);
+	PROPERTY(DWORD, unknown_40);
+	PROPERTY(int, unknown_44);
+	PROPERTY(DWORD, unknown_48);
+	PROPERTY(DWORD, unknown_4C);
+	PROPERTY(int, unknown_frame_50);
+	PROPERTY(DWORD, unknown_54);
+	PROPERTY_STRUCT(TimerStruct, unknown_timer_60);
+};
+
+#endif
