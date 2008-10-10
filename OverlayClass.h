0915c7fae611bd49b205f1074ad67a3ce26dff44
DCoder <dcoder1337@gmail.com>
2008-10-10 19:29:25 +0000

    Starting the enum of overlay hardcodings.
    git-svn-id: svn://svn.renegadeprojects.com/yrpp/trunk@193 bbc44d77-a14d-0410-bbfb-905795908d03

--- OverlayClass.h
+++ OverlayClass.h
@@ -5,6 +5,31 @@
 #ifndef OVERLAY_H
 #define OVERLAY_H
 
+/*
+
+;GEF in case it wasn't intuitively obvious from the above, this list mirrors an object enumeration
+;in overlay.hh. If you want to add something to this list, make sure you add to the enumeration
+;or get a programmer to do it for you
+
+ * Ironically, the list they had in overlay.hh doesn't seem to have been updated that much since TS.
+
+ */
+
+#define OVERLAY_VEINS 0x7E
+#define OVERLAY_VEINHOLE 0xA7
+#define OVERLAY_VEINHOLEDUMMY 0xB2
+
+#define OVERLAY_BRIDGEHEAD11 0x18
+#define OVERLAY_BRIDGEHEAD12 0x19
+
+#define OVERLAY_BRIDGEHEAD21 0xED
+#define OVERLAY_BRIDGEHEAD22 0xEE
+
+#define OVERLAY_LOBRIDGE1 0x7A
+#define OVERLAY_LOBRIDGE2 0x7B
+#define OVERLAY_LOBRIDGE3 0x7C
+#define OVERLAY_LOBRIDGE4 0x7D
+
 #include <ObjectClass.h>
 #include <OverlayTypeClass.h>
 
