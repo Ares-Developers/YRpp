fd13781c8443f470b912766eac5971c26fdb1cea
DCoder <dcoder1337@gmail.com>
2009-12-17 19:42:00 +0000

    Some updating
    Signed-off-by: DCoder <root@dc0d3r.name>
    
    git-svn-id: svn://svn.renegadeprojects.com/yrpp/trunk@254 bbc44d77-a14d-0410-bbfb-905795908d03

--- ObjectClass.h
+++ ObjectClass.h
@@ -100,7 +100,7 @@ public:
 	virtual bool vt_entry_BC(DWORD dwUnk) R0;
 	virtual bool vt_entry_C0() R0;
 	virtual bool IsDisguised() R0;
-	virtual bool IsDisguisedAs(HouseClass *target) R0;
+	virtual bool IsDisguisedAs(HouseClass *target) R0; // only works correctly on infantry!
 	virtual ObjectTypeClass* GetDisguise(bool DisguisedAgainstAllies) R0;
 	virtual HouseClass* GetDisguiseHouse(bool DisguisedAgainstAllies) R0;
 
@@ -110,6 +110,7 @@ public:
 	// place the object on the map
 	virtual bool Put(CoordStruct* pCrd, eDirection dFaceDir) R0;
 
+	// cleanup things (lose line trail, deselect, etc)
 	virtual void ReachedEndOfLife(bool Silently) RX;
 
 	virtual void RegisterDestruction(TechnoClass *Destroyer) RX;
@@ -137,26 +138,26 @@ public:
 	virtual RectangleStruct* vt_entry_12C(RectangleStruct* pRect) R0;
 	virtual void DrawRadialIndicator(DWORD dwUnk) RX;
 	virtual void vt_entry_134() RX;
-	virtual bool vt_entry_138() R0;
-	virtual bool vt_entry_13C() R0;
+	virtual bool CanBeSelected() R0;
+	virtual bool CanBeSelectedNow() R0;
 	virtual bool vt_entry_140(DWORD dwUnk, DWORD dwUnk2, DWORD dwUnk3, DWORD dwUnk4) R0;
-	virtual bool vt_entry_144(DWORD dwUnk, DWORD dwUnk2, DWORD dwUnk3) R0;
+	virtual bool ClickedAction(eAction Action, ObjectClass *Target, bool bUnk) R0;
 	virtual void Flash(int Duration) RX;
 	virtual bool Select() R0;
 	virtual void Deselect() RX;
 	virtual void IronCurtain(int nDuration, HouseClass *pSource, bool ForceShield) RX;
 	virtual void StopAirstrikeTimer() RX;
-	virtual void StopAirstrikeTimer(int Duration) RX;
+	virtual void StartAirstrikeTimer(int Duration) RX;
 	virtual bool IsIronCurtained() R0;
-	virtual bool vt_entry_164(DWORD dwUnk, DWORD dwUnk2) R0;
+	virtual bool IsCloseEnough3D(DWORD dwUnk, DWORD dwUnk2) R0;
 	virtual int GetWeaponRange(int idxWeapon) R0;
 	virtual eDamageState ReceiveDamage(int* pDamage, int DistanceFromEpicenter, WarheadTypeClass* pWH,
 	  ObjectClass* Attacker, bool IgnoreDefenses, bool PreventPassengerEscape, HouseClass* pAttackingHouse) R0;
-	virtual void FreeCaptured() RX;
+	virtual void Destroy() RX;
 	virtual void Scatter(DWORD dwUnk, DWORD dwUnk2, DWORD dwUnk3) RX;
 	virtual bool Ignite() R0;
 	virtual void vt_entry_17C() RX;
-	virtual DWORD vt_entry_180() R0;
+	virtual DWORD GetPointsValue() R0;
 	virtual eMission GetCurrentMission() R0;
 	virtual void vt_entry_188(DWORD dwUnk) RX;
 	virtual void UpdatePosition(int dwUnk) RX;
