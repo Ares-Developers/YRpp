659be33db1261e72eef278f52c1f9c88f5b6599c
DCoder1337 <dcoder1337@gmail.com>
2011-12-31 20:03:42 +0200

    Changes to support Syringe updates and .inj file removal: hook macros have been changed significantly, keep these changes in mind: - hook address and size now have to be specified as proper integers (with 0x prefix). - DEFINE_HOOK_AGAIN cannot be placed between DEFINE_HOOK and function body, as it's no longer a NOP macro. Either put it above DEFINE_HOOK or below function body. Do not mess with struct alignment.

--- Syringe.h
+++ Syringe.h
@@ -294,10 +294,46 @@ public:
 #define EXPORT extern "C" __declspec(dllexport) DWORD __cdecl
 #define EXPORT_FUNC(name) extern "C" __declspec(dllexport) DWORD __cdecl name (REGISTERS *R)
 
+#pragma pack(push, 16)
+#pragma warning(push)
+#pragma warning( disable : 4324)
+__declspec(align(16)) struct hookdecl {
+	unsigned int hookAddr;
+	unsigned int hookSize;
+	const char * hookName;
+};
+
+__declspec(align(16)) struct hostdecl {
+	unsigned int hostChecksum;
+	const char * hostName;
+};
+#pragma warning(pop)
+#pragma pack(pop)
+
+#pragma section(".syhks00", read, write)
+#pragma section(".syexe00", read, write)
+namespace SyringeData {
+	namespace Hooks {
+
+	};
+	namespace Hosts {
+
+	};
+};
+
+#define declhost(exename, checksum) \
+namespace SyringeData { namespace Hosts { __declspec(allocate(".syexe00")) hostdecl _hst__ ## exename = { checksum, #exename }; }; };
+
+#define declhook(hook, funcname, size) \
+namespace SyringeData { namespace Hooks { __declspec(allocate(".syhks00")) hookdecl _hk__ ## hook ## funcname = { hook, size, #funcname }; }; };
+
 // injgen
 // in addition to the injgen-declaration, also includes the function opening
-#define DEFINE_HOOK(hook, funcname, size) EXPORT_FUNC(funcname)
+#define DEFINE_HOOK(hook, funcname, size) \
+declhook(hook, funcname, size) \
+EXPORT_FUNC(funcname)
 // no function opening, use for injgen-decl when repeating the same hook at multiple addrs
-#define DEFINE_HOOK_AGAIN(hook, funcname, size)
+#define DEFINE_HOOK_AGAIN(hook, funcname, size) \
+declhook(hook, funcname, size)
 
 #endif
