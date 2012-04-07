45451d8f94bbca67f8752ed217675831ca7c2835
Zimmermann Gyula <graiondilach@hotmail.com>
2012-04-07 20:43:26 +0200

    Rewired UnitClass pointer back to TechnoClass for expanded use in Ares.

--- EBolt.h
+++ EBolt.h
@@ -36,7 +36,7 @@ public:
 	CoordStruct Point2;
 	DWORD unknown_18;	//Duration?
 	int Random;	//Random number between 0 and 256
-	UnitClass* Owner;
+	TechnoClass* Owner;	//ingame this is a UnitClass but needed to circumvent some issues
 	int WeaponSlot; // which weapon # to use from owner
 	int Lifetime; // this is >>= 1 each time DrawAll() is called, 0 => dtor (inline). Hi, welcome to dumb ideas.
 	bool AlternateColor;
