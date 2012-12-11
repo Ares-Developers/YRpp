6775543a83bfef25e7c94cd9c1f4ed5c612d4b3e
AlexB <Alexander-B@gmx.de>
2012-12-11 23:26:04 +0100

    Changed RandomDouble() to return a double from (0,1) instead of from [0,1]. This excludes 0 and 1. This should not shift the odds considerably, but it allows for certain assumptions.

--- Randomizer.h
+++ Randomizer.h
@@ -22,7 +22,7 @@ public:
 	// helper methods
 	double RandomDouble()
 	{
-		return this->RandomRanged(0, 2147483646) / 2147483646.0;
+		return this->RandomRanged(1, 2147483647) / 2147483648.0;
 	}
 
 	//Properties
