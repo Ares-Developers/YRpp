9ae0b6539a6a23d5a97cc9995456edf544a2f7c2
AlexB <Alexander-B@gmx.de>
2013-03-08 00:31:21 +0100

    Added PointerExpiredNotification.
    If a class creates an object of a type it usually wouldn't usually receive an invalid pointer notification, the class can subscribe to these notifications using this class.
    
    For example, if the ParasiteClass creates a squid grapple animation, it would not be notified when it is destroyed. Thus, it has to subscribe to NotifyInvalidAnim. When the grapple animation is destroyed (either by the ParasiteClass or in some external code), the ParasiteClass then unsubscribes from the NotifyInvalidAnim notifications.

---
+++ Notifications.h
@@ -0,0 +1,45 @@
+#ifndef YRPP_NOTIFICATIONS_H
+#define YRPP_NOTIFICATIONS_H
+
+#include <ArrayClasses.h>
+
+class AbstractClass;
+
+// encapsulates a bunch of vectors that are used for announcing invalid pointers.
+// if an AbstractClass is contained in a type list it will be notified through
+// PointerExpired, whenever an object of thas type expires.
+class PointerExpiredNotification {
+public:
+	static PointerExpiredNotification &NotifyInvalidObject; // Object class hierarchy
+	static PointerExpiredNotification &NotifyInvalidType; // AbstractType class hierarchy
+	static PointerExpiredNotification &NotifyInvalidAnim; // AnimClass
+	static PointerExpiredNotification &NotifyInvalidHouse; // HouseClass
+	static PointerExpiredNotification &NotifyInvalidTag; // TagClass
+	static PointerExpiredNotification &NotifyInvalidTrigger; // TriggerClass
+	static PointerExpiredNotification &NotifyInvalidFactory; // FactoryClass
+	static PointerExpiredNotification &NotifyInvalidWaypoint; // WaypointClass
+	static PointerExpiredNotification &NotifyInvalidTeam; // TeamClass
+	static PointerExpiredNotification &NotifyInvalidNeuron; // NeuronClass
+	static PointerExpiredNotification &NotifyInvalidActionOrEvent; // ActionClass and EventClass
+
+	inline bool Add(AbstractClass* object) {
+		// add only if doesn't exist
+		int index = this->Array.FindItemIndex(&object);
+		if(index < 0) {
+			return this->Array.AddItem(object);
+		}
+		return false;
+	}
+
+	inline bool Remove(AbstractClass* object) {
+		int index = this->Array.FindItemIndex(&object);
+		if(index > -1) {
+			return this->Array.RemoveItem(index);
+		}
+		return false;
+	}
+
+	DynamicVectorClass<AbstractClass*> Array;
+};
+
+#endif
