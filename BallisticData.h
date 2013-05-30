3ac10d52a2f708a457aed9c11b7e437bb4d14ec7
AlexB <Alexander-B@gmx.de>
2013-05-30 02:18:17 +0200

    Added BallisticData definition.
    BallisticData is responsible for encapsulating physical properties. These includes elasticity when bouncing off the ground, gravity, position, current velocity and terminal velocity, orientation and rotation.

---
+++ BallisticData.h
@@ -0,0 +1,56 @@
+/*
+	Ballistic trajectory data used by Animations and Voxel Animations.
+*/
+
+#ifndef BALLISTICDATA_H
+#define BALLISTICDATA_H
+
+#include <GeneralStructures.h>
+#include <YRMathVector.h>
+#include <Quaternion.h>
+
+class BallisticData
+{
+public:
+	class Status {
+		public:
+		typedef int Value;
+		enum {
+			None = 0,
+			Bounce = 1,
+			Impact = 2
+		};
+	};
+
+	// constructors
+	BallisticData() : Elasticity(0.0), Gravity(0.0), MaxVelocity(0.0) {}
+
+	BallisticData(CoordStruct* pCoords, double elasticity, double gravity,
+		double maxVelocity, Vector3D<float>* velocity, double angularVelocity)
+	{
+		this->Initialize(pCoords, elasticity, gravity, maxVelocity, velocity, angularVelocity);
+	}
+
+	void Initialize(CoordStruct* pCoords, double elasticity, double gravity,
+			double maxVelocity, Vector3D<float>* velocity, double angularVelocity)
+		{ JMP_THIS(0x4397E0); }
+
+	CoordStruct* GetCoords(CoordStruct* pBuffer) const
+		{ JMP_THIS(0x4399A0); }
+
+	Matrix3DStruct* GetDrawingMatrix(Matrix3DStruct* pBuffer) const
+		{ JMP_THIS(0x4399E0); }
+
+	Status::Value Update()
+		{ JMP_THIS(0x439B00); }
+
+	double Elasticity; // speed multiplier when bouncing off the ground
+	double Gravity; // subtracted from the Z coords every frame
+	double MaxVelocity; // 0.0 disables check
+	Vector3D<float> Coords; // position with precision
+	Vector3D<float> Velocity; // speed components
+	Quaternion CurrentAngle; // quaternion for drawing
+	Quaternion AngularVelocity; // second quaternion as per-frame delta
+};
+
+#endif
