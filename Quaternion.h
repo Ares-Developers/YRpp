ae336abff75a2ca6d954f36a28f666f714ba0cb8
AlexB <Alexander-B@gmx.de>
2013-05-30 02:13:52 +0200

    Added a Quaternion class stub.
    Quaternions use different math and support different operations than a hypothetical Vector4D<float>, so it would have been unnatural to make it a typedef.

---
+++ Quaternion.h
@@ -0,0 +1,17 @@
+#ifndef QUATERNION_H
+#define QUATERNION_H
+
+// an imaginary³ world where only one quarter is real.
+// obviously, this class needs some serious expansion
+// if it is to be used for serious stuff.
+
+class Quaternion
+{
+public:
+	Quaternion() : X(0.0f), Y(0.0f), Z(0.0f), W(1.0f) {}
+	Quaternion(float x, float y, float z, float w) : X(x), Y(y), Z(z), W(w) {}
+
+	float X, Y, Z, W; // W is the real part
+};
+
+#endif
