9f38774bd66cd4bef9a33e27c5f0d987ab7e55a1
AlexB <Alexander-B@gmx.de>
2013-10-20 00:35:10 +0200

    Added abstract_cast<T>
    Unifying two casts by adding a third one. abstract_cast uses generic_cast for a few hardcoded cases like ObjectClass, TechnoClass and FootClass (all MissionClass and RadioClass objects are TechnoClasses, so they get this behavior, too). In the other cases, specific_cast is used.
    
    abstract_cast can be used anywhere generic_cast or specific_cast is used. It has no overhead compared to the other two.

--- Helpers/Cast.h
+++ Helpers/Cast.h
@@ -3,6 +3,12 @@
 
 #include <AbstractClass.h>
 
+class ObjectClass;
+class MissionClass;
+class RadioClass;
+class TechnoClass;
+class FootClass;
+
 template <typename T>
 inline T specific_cast(AbstractClass* pAbstract) {
 	if(pAbstract && pAbstract->WhatAmI() == CompoundT<T>::BaseT::AbsID) {
@@ -17,6 +23,41 @@ inline T generic_cast(AbstractClass* pAbstract) {
 		return reinterpret_cast<T>(pAbstract);
 	}
 	return nullptr;
+};
+
+template <typename T>
+inline T abstract_cast(AbstractClass* pAbstract) {
+	return specific_cast<T>(pAbstract);
+};
+
+template <>
+inline AbstractClass* abstract_cast<AbstractClass*>(AbstractClass* pAbstract) {
+	return pAbstract;
+};
+
+template <>
+inline ObjectClass* abstract_cast<ObjectClass*>(AbstractClass* pAbstract) {
+	return generic_cast<ObjectClass*>(pAbstract);
+};
+
+template <>
+inline MissionClass* abstract_cast<MissionClass*>(AbstractClass* pAbstract) {
+	return reinterpret_cast<MissionClass*>(generic_cast<TechnoClass*>(pAbstract));
+};
+
+template <>
+inline RadioClass* abstract_cast<RadioClass*>(AbstractClass* pAbstract) {
+	return reinterpret_cast<RadioClass*>(generic_cast<TechnoClass*>(pAbstract));
+};
+
+template <>
+inline TechnoClass* abstract_cast<TechnoClass*>(AbstractClass* pAbstract) {
+	return generic_cast<TechnoClass*>(pAbstract);
+};
+
+template <>
+inline FootClass* abstract_cast<FootClass*>(AbstractClass* pAbstract) {
+	return generic_cast<FootClass*>(pAbstract);
 };
 
 #endif
