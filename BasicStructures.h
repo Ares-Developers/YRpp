0b5ecf3f6848a8d8d5afa66727e13aecd0f0f201
AlexB <Alexander-B@gmx.de>
2014-01-24 15:18:51 +0100

    Extracted some of the basic structs from GeneralStructures.

---
+++ BasicStructures.h
@@ -0,0 +1,101 @@
+#ifndef BASICSTRUCTURES_H
+#define BASICSTRUCTURES_H
+
+//used for most colors
+struct ColorStruct
+{
+	BYTE R, G, B;
+
+	void operator = (ColorStruct Color)
+	{
+		R = Color.R;
+		G = Color.G;
+		B = Color.B;
+	}
+
+	bool operator == (ColorStruct rhs)
+	{
+		return R == rhs.R && G == rhs.G && B == rhs.B;
+	}
+
+	ColorStruct()
+	{
+	}
+
+	ColorStruct(BYTE _R, BYTE _G, BYTE _B)
+	{
+		R = _R;
+		G = _G;
+		B = _B;
+	}
+};
+
+#ifndef PALETTE_STRUCT
+#define PALETTE_STRUCT
+struct BytePalette {
+	ColorStruct Entries[256];
+	ColorStruct & operator [](const int idx) {
+		return this->Entries[idx];
+	}
+};
+#endif
+
+//used for light colors
+struct TintStruct
+{
+	int Red, Green, Blue;
+};
+
+//16bit colors
+#pragma pack(push, 1)
+struct Color16Struct
+{
+	int R : 5;
+	int G : 6;
+	int B : 5;
+
+	void operator = (Color16Struct Color)
+	{
+		R = Color.R;
+		G = Color.G;
+		B = Color.B;
+	}
+
+	void operator = (ColorStruct Color)
+	{
+		R = Color.R >> 3;
+		G = Color.G >> 2;
+		B = Color.B >> 3;
+	}
+
+	bool operator == (Color16Struct rhs)
+	{
+		return R == rhs.R && G == rhs.G && B == rhs.B;
+	}
+
+	Color16Struct()
+	{
+	}
+};
+#pragma pack(pop)
+
+
+//Random number range
+struct RandomStruct
+{
+	int Min, Max;
+};
+
+//3D Matrix
+struct Matrix3DStruct
+{
+	float Data[12];
+};
+
+//obvious
+struct RectangleStruct
+{
+	int X, Y, Width, Height;
+};
+
+#endif
--- GeneralStructures.h
+++ GeneralStructures.h
@@ -5,6 +5,7 @@
 #include <YRPPCore.h>
 #include <YRMathVector.h>
 #include <GeneralDefinitions.h> // need eDirection for FacingStruct
+#include <BasicStructures.h>
 
 #include <string.h>
 
@@ -19,83 +20,6 @@ typedef Vector3D<int> CoordStruct;
 typedef Vector3D<BYTE> RGBClass; // <pd> wuhaha
 #endif
 
-//used for most colors
-struct ColorStruct
-{
-	BYTE R, G, B;
-
-	void operator = (ColorStruct Color)
-	{
-		R = Color.R;
-		G = Color.G;
-		B = Color.B;
-	}
-
-	bool operator == (ColorStruct rhs)
-	{
-		return R == rhs.R && G == rhs.G && B == rhs.B;
-	}
-
-	ColorStruct()
-	{
-	}
-
-	ColorStruct(BYTE _R, BYTE _G, BYTE _B)
-	{
-		R = _R;
-		G = _G;
-		B = _B;
-	}
-};
-
-#ifndef PALETTE_STRUCT
-#define PALETTE_STRUCT
-struct BytePalette {
-	ColorStruct Entries[256];
-	ColorStruct & operator [](const int idx) {
-		return this->Entries[idx];
-	}
-};
-#endif
-
-//used for light colors
-struct TintStruct
-{
-	int Red, Green, Blue;
-};
-
-//16bit colors
-#pragma pack(push, 1)
-struct Color16Struct
-{
-	int R:5;
-	int G:6;
-	int B:5;
-
-	void operator = (Color16Struct Color)
-	{
-		R = Color.R;
-		G = Color.G;
-		B = Color.B;
-	}
-
-	void operator = (ColorStruct Color)
-	{
-		R = Color.R >> 3;
-		G = Color.G >> 2;
-		B = Color.B >> 3;
-	}
-
-	bool operator == (Color16Struct rhs)
-	{
-		return R == rhs.R && G == rhs.G && B == rhs.B;
-	}
-
-	Color16Struct()
-	{
-	}
-};
-#pragma pack(pop)
 
 //uses the clock values
 struct DirStruct
@@ -119,24 +43,6 @@ struct DirStruct
 	Raw Value;
 private:
 	WORD unused_2;
-};
-
-//Random numbe range
-struct RandomStruct
-{
-	int Min, Max;
-};
-
-//3D Matrix
-struct Matrix3DStruct
-{
-	float Data[12];
-};
-
-//obvious
-struct RectangleStruct
-{
-	int X, Y, Width, Height;
 };
 
 //used for timed events, time measured in frames!
