641cf478da16c12bb4d590f399391beafc5f24da
AlexB <Alexander-B@gmx.de>
2014-02-22 11:03:47 +0100

    IsKeyPressed() method for InputManagerClass
    Also added helpers that check whether any Force Fire, Force Move or Force Select buttons are pressed.

--- InputManagerClass.h
+++ InputManagerClass.h
@@ -1,12 +1,32 @@
 #ifndef INPUTMGR_H
 #define INPUTMGR_H
 
+#include <GameOptionsClass.h>
+
 class InputManagerClass {
 public:
 	static InputManagerClass * &Instance;
 
 	byte DoSomething()
 		{ JMP_THIS(0x54F720); }
+
+	bool IsKeyPressed(int key) const
+		{ JMP_THIS(0x54F5C0); }
+
+	bool IsForceFireKeyPressed() const {
+		return this->IsKeyPressed(GameOptionsClass::Instance->KeyForceFire1)
+			|| this->IsKeyPressed(GameOptionsClass::Instance->KeyForceFire2);
+	}
+
+	bool IsForceMoveKeyPressed() const {
+		return this->IsKeyPressed(GameOptionsClass::Instance->KeyForceMove1)
+			|| this->IsKeyPressed(GameOptionsClass::Instance->KeyForceMove2);
+	}
+
+	bool IsForceSelectKeyPressed() const {
+		return this->IsKeyPressed(GameOptionsClass::Instance->KeyForceSelect1)
+			|| this->IsKeyPressed(GameOptionsClass::Instance->KeyForceSelect2);
+	}
 
 protected:
   int field_0;
