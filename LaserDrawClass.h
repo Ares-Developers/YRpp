da62cc14ce9594ec81c6c4dfacde5a0f07d87e37
AlexB <Alexander-B@gmx.de>
2014-07-05 22:09:39 +0200

    Update for LaserDrawClass.

--- LaserDrawClass.h
+++ LaserDrawClass.h
@@ -12,33 +12,19 @@ class LaserDrawClass
 {
 public:
 	//Constructor, Destructor
-	LaserDrawClass( CoordStruct P1,
-					CoordStruct P2,
-					ColorStruct InnerColor,
-					ColorStruct OuterColor,
-					ColorStruct OuterSpread,
-					int nDuration)
-	{
-		PUSH_IMM(0); //???
-		PUSH_IMM(0x3FF00000); //???
-		PUSH_IMM(1); //???
-		PUSH_IMM(0); //???
-		PUSH_VAR32(nDuration);
-		PUSH_VAR32(OuterSpread);
-		PUSH_VAR32(OuterColor);
-		PUSH_VAR32(InnerColor);
-		PUSH_VAR32(1); //??? bool
-		PUSH_VAR32(0); //cutoff
-		int X,Y,Z;
-		X = P2.X; Y = P2.Y; Z = P2.Z;
-		PUSH_VAR32(Z); PUSH_VAR32(Y); PUSH_VAR32(X);
-		X = P1.X; Y = P1.Y; Z = P1.Z;
-		PUSH_VAR32(Z); PUSH_VAR32(Y); PUSH_VAR32(X);
-		THISCALL(0x54FE60);
-	}
+	LaserDrawClass(const CoordStruct& source, const CoordStruct& target, const ColorStruct& innerColor,
+		const ColorStruct& outerColor, const ColorStruct& outerSpread, int duration)
+			: LaserDrawClass(source, target, 0, 1, innerColor, outerColor, outerSpread, duration)
+	{ }
 
+	LaserDrawClass(CoordStruct source, CoordStruct target, int zAdjust, BYTE unknown,
+		ColorStruct innerColor, ColorStruct outerColor, ColorStruct outerSpread,
+		int duration, bool blinks = false, bool fades = true,
+		float startIntensity = 1.0f, float endIntensity = 0.0f)
+		{ JMP_THIS(0x54FE60); }
 
-	~LaserDrawClass() {/*???*/}
+	~LaserDrawClass()
+		{ JMP_THIS(0x54FFB0); }
 
 	//===========================================================================
 	//===== Properties ==========================================================
@@ -48,24 +34,22 @@ public:
 	int Thickness; // only respected if IsHouseColor
 	bool IsHouseColor;
 	char field_21;
-	char field_22;
-	char field_23;
+	PROTECTED_PROPERTY(BYTE, align_22[2]);
 	CoordStruct Source;
 	CoordStruct Target;
-	int field_3C;
+	int ZAdjust;
 	char field_40;
 	ColorStruct InnerColor;
 	ColorStruct OuterColor;
 	ColorStruct OuterSpread;
-	char field_4A;
-	char field_4B;
+	PROTECTED_PROPERTY(BYTE, align_4A[2]);
 	int Duration;
-	char Blinks;
-	char BlinkState;
-	char field_52;
-	char field_53;
-	float floatVal;
-	int field_58;
+	bool Blinks;
+	bool BlinkState;
+	bool Fades;
+	PROTECTED_PROPERTY(BYTE, align_53);
+	float StartIntensity;
+	float EndIntensity;
 };
 
 #endif
