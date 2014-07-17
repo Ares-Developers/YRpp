a5ec9365654f6765fdd69aa3d3d0d6df5ec16fb5
AlexB <Alexander-B@gmx.de>
2014-07-17 17:15:46 +0200

    Implemented the Checksummer, removed calls of game methods
    This implementation has no out of bounds writes beyond the class bounds. See the added check in the Fill method for ByteIndex == Size.
    
    Several functions are split into a public method and an internal inline function. This is to prevent lots of function calls in methods that are called thousands of times potentially.
    
    The Checksummer now accepts all values without casting. There is a function template that takes care of all types, and calls a single processing function, so not every type gets its own instantiation of the big function.
    
    The current value can be retrieved using GetValue(), which fills the current buffer and also considers its value. Immediate() returns the raw value not considering the current buffer. Commit() fills the current buffer, processes and then clears it. Calling GetValue() before Commit() returns the same value as Immediate() after Commit(). GetValue() doesn't change the internal state, but it is not bitwise const as it fills the buffer.

--- Checksummer.h
+++ Checksummer.h
@@ -4,64 +4,152 @@
 #include <GeneralDefinitions.h>
 
 class Checksummer {
+	static const size_t Size = 4;
 public:
-	void Add(BYTE Value)
-		{ JMP_THIS(0x4A1C10); };
+	Checksummer() : Value(0), ByteIndex(0) {
+		for(int i = 0; i < Size; ++i) {
+			this->Bytes[i] = 0;
+		}
+	}
 
-	void Add(bool Value)
-		{ JMP_THIS(0x4A1CA0); };
+	DWORD GetValue() const {
+		return this->GetValueInline();
+	}
 
-	void Add(WORD Value)
-		{ JMP_THIS(0x4A1D30); };
+	DWORD Intermediate() const {
+		return this->Value;
+	}
 
-	void Add(DWORD Value)
-		{ JMP_THIS(0x4A1D50); };
+	void Commit() {
+		this->CommitInline();
+	}
 
-	void Add(float Value)
-		{ JMP_THIS(0x4A1D70); };
+	void Add(const void* data, size_t c_bytes) {
+		if(data && c_bytes) {
+			auto bytes = reinterpret_cast<const BYTE*>(data);
 
-	void Add(double Value)
-		{ JMP_THIS(0x4A1D90); };
+			// fill the current block
+			while(this->ByteIndex != 0 && this->ByteIndex < Size && c_bytes) {
+				this->AddInline(*bytes++);
+				--c_bytes;
+			}
 
-	void Add(const char * Value)
-		{ JMP_THIS(0x4A1DB0); };
+			// take the full blocks
+			const auto blocks = c_bytes / Size;
+			for(auto i = 0u; i < blocks; ++i) {
+				this->Value = Process(bytes, Size, this->Value);
+				bytes += Size;
+			}
+			c_bytes -= blocks * Size;
 
-	void Add(const BYTE * Buffer, int Length)
-		{ JMP_THIS(0x4A1DE0); };
+			// fill in the remainder
+			while(c_bytes--) {
+				this->AddInline(*bytes++);
+			}
+		}
+	}
+
+	void Add(BYTE value) {
+		this->AddInline(value);
+	}
+
+	void Add(bool value) {
+		this->Add(static_cast<BYTE>(value != 0));
+	}
+
+	void Add(char value) {
+		this->Add(static_cast<BYTE>(value));
+	}
+
+	void Add(signed char value) {
+		this->Add(static_cast<BYTE>(value));
+	}
+
+	void Add(const char* string) {
+		if(string) {
+			this->Add(string, strlen(string));
+		}
+	}
+
+	template <typename T>
+	void Add(const T& value) {
+		this->Add(&value, sizeof(T));
+	}
+
+	static DWORD Process(const void* data, size_t size, DWORD initial) {
+		auto bytes = reinterpret_cast<const BYTE*>(data);
+		auto ret = ~initial;
+
+		static const DWORD* CRC_Table = reinterpret_cast<DWORD*>(0x81F7B4);
+
+		for(auto i = 0u; i < size; ++i) {
+			ret = CRC_Table[*bytes++ ^ static_cast<BYTE>(ret)] ^ (ret >> 8);
+		}
+
+		return ~ret;
+	}
+
+	static DWORD Process(const char* string, DWORD initial) {
+		auto bytes = reinterpret_cast<const BYTE*>(string);
+		auto ret = ~initial;
+
+		static auto CRC_Table = reinterpret_cast<const DWORD*>(0x81F7B4);
+
+		while(*bytes) {
+			ret = CRC_Table[*bytes++ ^ static_cast<BYTE>(ret)] ^ (ret >> 8);
+		}
+
+		return ~ret;
+	}
 
 protected:
-	DWORD AddBuffer(const BYTE * Buffer, int Length, DWORD InitialValue)
-		{ JMP_THIS(0x4A1C10); };
+	void Fill() const {
+		// this check is missing in the original, which makes it
+		// write beyond the array, ie. outside the class' memory.
+		if(this->ByteIndex < Size) {
+			this->Bytes[this->ByteIndex] = static_cast<BYTE>(this->ByteIndex);
+			for(auto i = this->ByteIndex + 1; i < Size; ++i) {
+				this->Bytes[i] = this->Bytes[0];
+			}
+		}
+	}
 
-public:
-	void Reset() {
-		CurrentValue = 0;
-		CurrentByteIndex = 0;
-		CurrentBytes[0] = 0;
-		CurrentBytes[1] = 0;
-		CurrentBytes[2] = 0;
-		CurrentBytes[3] = 0;
-		LastByteIndex = 0;
-	};
+	__forceinline void AddInline(BYTE value) {
+		// clear old data
+		if(this->ByteIndex == 0) {
+			for(int i = 0; i < Size; ++i) {
+				this->Bytes[i] = 0;
+			}
+		}
 
-	Checksummer() : CurrentValue(0), CurrentByteIndex(0), LastByteIndex(0) {
-		CurrentBytes[0] = 0;
-		CurrentBytes[1] = 0;
-		CurrentBytes[2] = 0;
-		CurrentBytes[3] = 0;
-	};
+		this->Bytes[this->ByteIndex++] = value;
 
-	DWORD CurrentValue;
-	int   CurrentByteIndex;
-	BYTE  CurrentBytes[4];
+		if(this->ByteIndex == Size) {
+			this->CommitInline();
+		}
+	}
 
-	/*
-	 * this is not entirely correct
-	 * the original class doesn't have this member and as such its sizeof == 0xC, but the code writes to the 0xC'th byte anyway...
-	 * when the class is allocated through the heap, it works because windows apparently aligns memory blocks to 8 byte boundaries
-	 * but when it's allocated on the stack, all hell breaks loose
-	 */
-	BYTE  LastByteIndex;
+	__forceinline DWORD GetValueInline() const {
+		// nothing to check
+		if(!this->ByteIndex) {
+			return this->Value;
+		}
+
+		// fill the remaining bytes
+		this->Fill();
+
+		// project the value without changing internal state
+		return Process(this->Bytes, Size, this->Value);
+	}
+
+	__forceinline void CommitInline() {
+		this->Value = this->GetValueInline();
+		this->ByteIndex = 0;
+	}
+
+	DWORD Value;
+	size_t ByteIndex;
+	mutable BYTE Bytes[Size];
 };
 
 #endif
