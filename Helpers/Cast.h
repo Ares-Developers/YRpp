4e712fef2064f7c5810ff8037f4fcc74af266715
AlexB <Alexander-B@gmx.de>
2014-10-11 01:41:27 +0200

    Added support for pointers to const to generic_cast, specific_cast and abstract_cast.

--- Helpers/Cast.h
+++ Helpers/Cast.h
@@ -15,6 +15,16 @@ template <typename T>
 inline T specific_cast(AbstractClass* pAbstract) {
 	using Base = std::remove_pointer_t<T>;
 
+	return const_cast<Base*>(specific_cast<const Base*>(static_cast<const AbstractClass*>(pAbstract)));
+};
+
+template <typename T>
+inline T specific_cast(const AbstractClass* pAbstract) {
+	using Base = std::remove_const_t<std::remove_pointer_t<T>>;
+
+	static_assert(std::is_const<std::remove_pointer_t<T>>::value,
+		"specific_cast: T is required to be const.");
+
 	static_assert(std::is_base_of<AbstractClass, Base>::value,
 		"specific_cast: T is required to be a type derived from AbstractClass.");
 
@@ -31,6 +41,16 @@ template <typename T>
 inline T generic_cast(AbstractClass* pAbstract) {
 	using Base = std::remove_pointer_t<T>;
 
+	return const_cast<Base*>(generic_cast<const Base*>(static_cast<const AbstractClass*>(pAbstract)));
+};
+
+template <typename T>
+inline T generic_cast(const AbstractClass* pAbstract) {
+	using Base = std::remove_const_t<std::remove_pointer_t<T>>;
+
+	static_assert(std::is_const<std::remove_pointer_t<T>>::value,
+		"generic_cast: T is required to be const.");
+
 	static_assert(std::is_base_of<ObjectClass, Base>::value
 		&& std::is_abstract<Base>::value,
 		"generic_cast: T is required to be an abstract type derived from ObjectClass.");
@@ -45,6 +65,16 @@ template <typename T>
 inline T abstract_cast(AbstractClass* pAbstract) {
 	using Base = std::remove_pointer_t<T>;
 
+	return const_cast<T>(abstract_cast<const Base*>(static_cast<const AbstractClass*>(pAbstract)));
+};
+
+template <typename T>
+inline T abstract_cast(const AbstractClass* pAbstract) {
+	using Base = std::remove_const_t<std::remove_pointer_t<T>>;
+
+	static_assert(std::is_const<std::remove_pointer_t<T>>::value,
+		"abstract_cast: T is required to be const.");
+
 	static_assert(std::is_base_of<AbstractClass, Base>::value,
 		"abstract_cast: T is required to be a type derived from AbstractClass.");
 
@@ -54,6 +84,8 @@ inline T abstract_cast(AbstractClass* pAbstract) {
 
 	return specific_cast<T>(pAbstract);
 };
+
+// non-const versions
 
 template <>
 inline AbstractClass* abstract_cast<AbstractClass*>(AbstractClass* pAbstract) {
@@ -83,6 +115,38 @@ inline TechnoClass* abstract_cast<TechnoClass*>(AbstractClass* pAbstract) {
 template <>
 inline FootClass* abstract_cast<FootClass*>(AbstractClass* pAbstract) {
 	return generic_cast<FootClass*>(pAbstract);
+};
+
+// const versions
+
+template <>
+inline const AbstractClass* abstract_cast<const AbstractClass*>(const AbstractClass* pAbstract) {
+	return pAbstract;
+};
+
+template <>
+inline const ObjectClass* abstract_cast<const ObjectClass*>(const AbstractClass* pAbstract) {
+	return generic_cast<const ObjectClass*>(pAbstract);
+};
+
+template <>
+inline const MissionClass* abstract_cast<const MissionClass*>(const AbstractClass* pAbstract) {
+	return reinterpret_cast<const MissionClass*>(generic_cast<const TechnoClass*>(pAbstract));
+};
+
+template <>
+inline const RadioClass* abstract_cast<const RadioClass*>(const AbstractClass* pAbstract) {
+	return reinterpret_cast<const RadioClass*>(generic_cast<const TechnoClass*>(pAbstract));
+};
+
+template <>
+inline const TechnoClass* abstract_cast<const TechnoClass*>(const AbstractClass* pAbstract) {
+	return generic_cast<const TechnoClass*>(pAbstract);
+};
+
+template <>
+inline const FootClass* abstract_cast<const FootClass*>(const AbstractClass* pAbstract) {
+	return generic_cast<const FootClass*>(pAbstract);
 };
 
 #endif
