7097b2ac920ae69d8601b1d56a761d2aa15bdf6a
AlexB <Alexander-B@gmx.de>
2014-11-01 00:16:15 +0100

    A machinery to add support for bitwise operations to certain enum classes.
    Just define the enum class, then use MAKE_ENUM_FLAGS(typename) to enable support. It is kinda flaky, because if(enumvalue) still does not work. Use if(!!enumvalue) for now.

---
+++ Helpers/EnumFlags.h
@@ -0,0 +1,86 @@
+#ifndef HELPER_ENUMFLAG_H
+#define HELPER_ENUMFLAG_H
+
+#include <type_traits>
+
+template <typename T>
+struct EnumFlagHelper {
+	using type = std::underlying_type_t<T>;
+
+	EnumFlagHelper(T value) {
+		this->value = static_cast<type>(value);
+	}
+
+	explicit operator bool() const {
+		return value != type();
+	}
+
+	operator T() const {
+		return static_cast<T>(this->value);
+	}
+
+	explicit operator type() const {
+		return value;
+	}
+
+private:
+
+	type value;
+};
+
+#define MAKE_ENUM_FLAGS(FLAG_ENUM_NAME) \
+	inline EnumFlagHelper<FLAG_ENUM_NAME> operator& (EnumFlagHelper<FLAG_ENUM_NAME> lhs, EnumFlagHelper<FLAG_ENUM_NAME> rhs) { \
+		using type = std::underlying_type_t<FLAG_ENUM_NAME>; \
+		return static_cast<FLAG_ENUM_NAME>(static_cast<type>(lhs) & static_cast<type>(rhs)); \
+	} \
+	\
+	inline EnumFlagHelper<FLAG_ENUM_NAME> operator& (EnumFlagHelper<FLAG_ENUM_NAME> lhs, FLAG_ENUM_NAME rhs) { \
+		return lhs & EnumFlagHelper<FLAG_ENUM_NAME>(rhs); \
+	} \
+	\
+	inline EnumFlagHelper<FLAG_ENUM_NAME> operator& (FLAG_ENUM_NAME lhs, EnumFlagHelper<FLAG_ENUM_NAME> rhs) { \
+		return EnumFlagHelper<FLAG_ENUM_NAME>(lhs) & rhs; \
+	} \
+	\
+	inline EnumFlagHelper<FLAG_ENUM_NAME> operator& (FLAG_ENUM_NAME lhs, FLAG_ENUM_NAME rhs) { \
+		return EnumFlagHelper<FLAG_ENUM_NAME>(lhs) & rhs; \
+	} \
+	\
+	inline FLAG_ENUM_NAME& operator&= (FLAG_ENUM_NAME &lhs, FLAG_ENUM_NAME rhs) { \
+		return lhs = EnumFlagHelper<FLAG_ENUM_NAME>(lhs) & rhs; \
+	} \
+	\
+	\
+	inline EnumFlagHelper<FLAG_ENUM_NAME> operator| (EnumFlagHelper<FLAG_ENUM_NAME> lhs, EnumFlagHelper<FLAG_ENUM_NAME> rhs) { \
+		using type = std::underlying_type_t<FLAG_ENUM_NAME>; \
+		return static_cast<FLAG_ENUM_NAME>(static_cast<type>(lhs) | static_cast<type>(rhs)); \
+	} \
+	\
+	inline EnumFlagHelper<FLAG_ENUM_NAME> operator| (EnumFlagHelper<FLAG_ENUM_NAME> lhs, FLAG_ENUM_NAME rhs) { \
+		return lhs | EnumFlagHelper<FLAG_ENUM_NAME>(rhs); \
+	} \
+	\
+	inline EnumFlagHelper<FLAG_ENUM_NAME> operator| (FLAG_ENUM_NAME lhs, EnumFlagHelper<FLAG_ENUM_NAME> rhs) { \
+		return EnumFlagHelper<FLAG_ENUM_NAME>(lhs) | rhs; \
+	} \
+	 \
+	inline EnumFlagHelper<FLAG_ENUM_NAME> operator| (FLAG_ENUM_NAME lhs, FLAG_ENUM_NAME rhs) { \
+		return EnumFlagHelper<FLAG_ENUM_NAME>(lhs) | rhs; \
+	} \
+	\
+	inline FLAG_ENUM_NAME& operator|= (FLAG_ENUM_NAME &lhs, FLAG_ENUM_NAME rhs) { \
+		return lhs = EnumFlagHelper<FLAG_ENUM_NAME>(lhs) | rhs; \
+	} \
+	\
+	\
+	inline EnumFlagHelper<FLAG_ENUM_NAME> operator~ (FLAG_ENUM_NAME rhs) { \
+		using type = std::underlying_type_t<FLAG_ENUM_NAME>; \
+		return static_cast<FLAG_ENUM_NAME>(~static_cast<type>(rhs)); \
+	} \
+	\
+	inline bool operator! (FLAG_ENUM_NAME rhs) { \
+		using type = std::underlying_type_t<FLAG_ENUM_NAME>; \
+		return !static_cast<type>(rhs); \
+	} \
+
+#endif
