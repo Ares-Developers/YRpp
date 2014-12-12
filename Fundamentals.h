1606cf39cf8ac61df4b1856b5bdc14331b26765f
AlexB <Alexander-B@gmx.de>
2014-12-12 02:02:21 +0100

    Added LevelHeight constant with some explanation.

--- Fundamentals.h
+++ Fundamentals.h
@@ -8,6 +8,12 @@ included for them to be available.
 namespace Unsorted
 {
 	static const int& CurrentFrame = *reinterpret_cast<int*>(0xA8ED84);
+
+	// The height in the middle of a cell with a slope of 30 degrees
+	const int LevelHeight = 104;
+	// tan(deg2rad(90) - deg2rad(60)) * sqrt(2 * 256 ^ 2) * 0.5
+	// cot(deg2rad(30)) * diagonal_leptons_per_cell * 0.5
+	// sqrt(3)/3 * 362.038 * 0.5
 }
 
 #endif
