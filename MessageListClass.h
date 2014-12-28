a20fe645ced21bffefb6293c30005c376a7de258
AlexB <Alexander-B@gmx.de>
2014-12-28 04:59:48 +0100

    MessageListClass redefined
    Removed two overloads, added two new. The duration is kinda important, so it can be passed as a number of frames as integer or as minutes as a double.

--- MessageListClass.h
+++ MessageListClass.h
@@ -12,21 +12,20 @@
 class MessageListClass
 {
 public:
-	static MessageListClass * Instance;
+	static MessageListClass* Instance;
 
 	// if pLabel is given, the message will be {$pLabel}:{$pMessage}
 	// else it will be just {$pMessage}
 
-	void PrintMessage(const wchar_t * pLabel, DWORD dwUnk1, const wchar_t* pMessage, int nColorSchemeIndex = 3,
-		DWORD dwUnk2 = 0x4046, DWORD dwUnk3 = 0x96, bool bSilent = false)
+	void PrintMessage(const wchar_t* pLabel, DWORD dwUnk1, const wchar_t* pMessage, int nColorSchemeIndex = ColorScheme::Yellow,
+		DWORD dwUnk2 = 0x4046, int duration = 0x96, bool bSilent = false)
 		{ JMP_THIS(0x5D3BA0); };
 
-	void PrintMessage(const wchar_t* pLabel, const wchar_t* pMessage, int nColorSchemeIndex = ColorScheme::White)
-		{ this->PrintMessage(pLabel, 0, pMessage, nColorSchemeIndex); }
+	void PrintMessage(const wchar_t* pMessage, int durationFrames = 0x96, int nColorSchemeIndex = ColorScheme::White, bool bSilent = false)
+		{ this->PrintMessage(nullptr, 0, pMessage, nColorSchemeIndex, 0x4046, durationFrames, bSilent); }
 
-	void PrintMessage(const wchar_t* pMessage, int nColorSchemeIndex = ColorScheme::White)
-		{ this->PrintMessage(nullptr, 0, pMessage, nColorSchemeIndex); }
-
+	void PrintMessage(const wchar_t* pMessage, double durationMinutes, int nColorSchemeIndex = ColorScheme::White, bool bSilent = false)
+		{ this->PrintMessage(nullptr, 0, pMessage, nColorSchemeIndex, 0x4046, static_cast<int>(durationMinutes * 900), bSilent); }
 };
 
 #endif
