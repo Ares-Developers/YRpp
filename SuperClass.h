aaf2a36c070321831b80b0e7386bd67564b8c44e
AlexB <Alexander-B@gmx.de>
2015-01-26 15:04:43 +0100

    Increased consistency. Also changed some methods to take const references.

--- SuperClass.h
+++ SuperClass.h
@@ -34,14 +34,14 @@ public:
 	virtual ~SuperClass() RX;
 
 	// non virtual
-	void CreateChronoAnim(CoordStruct Coords)
+	void CreateChronoAnim(CoordStruct coords)
 		{ JMP_THIS(0x6CB3A0); }
 
 	void Reset()
 		{ JMP_THIS(0x6CE0B0); }
 
 	// was the setting successful? (did we have to change the state (true) or was it already in the same hold state(false))
-	bool SetOnHold(bool status)
+	bool SetOnHold(bool onHold)
 		{ JMP_THIS(0x6CB4D0); }
 
 	bool Grant(bool oneTime, bool announce, bool onHold)
@@ -54,28 +54,28 @@ public:
 	bool IsPowered() const
 		{ return this->Type->IsPowered; }
 
-	void Launch(CellStruct *Cell, bool IsPlayer)
+	void Launch(const CellStruct& cell, bool isPlayer)
 		{ JMP_THIS(0x6CC390); }
 
 	char CanFire() const
 		{ JMP_THIS(0x6CC360); }
 
-	void SetReadiness(bool Ready) // this->IsCharged = Ready
+	void SetReadiness(bool ready) // this->IsCharged = Ready
 		{ JMP_THIS(0x6CB820); }
 
-	char StopPreclickAnim(bool /*IsPlayer*/) // if this is a PostClick SW, firing it does this
+	char StopPreclickAnim(bool isPlayer) // if this is a PostClick SW, firing it does this
 		{ JMP_THIS(0x6CB830); }
 
-	char ClickFire(bool IsPlayer, CellStruct *Coords) // calls Launch after printing Lightning Storm warning and other fluff
+	char ClickFire(bool isPlayer, const CellStruct& cell) // calls Launch after printing Lightning Storm warning and other fluff
 		{ JMP_THIS(0x6CB920); }
 
-	bool HasChargeProgressed(bool IsPlayer) // true if the charge has changed (charge overlay on the cameo)
+	bool HasChargeProgressed(bool isPlayer) // true if the charge has changed (charge overlay on the cameo)
 		{ JMP_THIS(0x6CBCA0); }               // triggers the EVA Announcement if it's ready
 
 	signed int GetCameoChargeState() const // which cameo charge overlay frame to show
 		{ JMP_THIS(0x6CBEE0); }
 
-	void SetCharge(int Percentage)
+	void SetCharge(int percentage)
 		{ JMP_THIS(0x6CC1E0); }
 
 	int GetRechargeTime() const // the time it takes this SW to recharge fully
@@ -87,7 +87,7 @@ public:
 	void ResetRechargeTime() // nullifies the previous call
 		{ JMP_THIS(0x6CC290); }
 
-	wchar_t *NameReadiness() const // the string to be displayed over the SW in the sidebar - "Ready" or ChargeDrain state
+	const wchar_t* NameReadiness() const // the string to be displayed over the SW in the sidebar - "Ready" or ChargeDrain state
 		{ JMP_THIS(0x6CC2B0); }
 
 	bool ShouldDrawProgress() const // sidebar
@@ -124,7 +124,7 @@ public:
 	PROTECTED_PROPERTY(BYTE, unused_61);
 	CellStruct ChronoMapCoords;  // 0x62
 	PROTECTED_PROPERTY(BYTE, unused_66[2]);
-	AnimClass * Animation;                // 0x68
+	AnimClass* Animation;                // 0x68
 	bool AnimationGotInvalid;
 	bool Granted;
 	bool OneTime; // remove this SW when it has been fired once
@@ -153,8 +153,8 @@ public:
 	static DynamicVectorClass<AnimClass*>* CloudsManifesting;
 	static DynamicVectorClass<AnimClass*>* BoltsPresent;
 
-	static void Start(int Duration, int Deferment, CellStruct Coords, HouseClass *pOwner)
-		{ PUSH_VAR32(pOwner); PUSH_VAR32(Coords); SET_REG32(EDX, Deferment); SET_REG32(ECX, Duration);
+	static void Start(int duration, int deferment, CellStruct cell, HouseClass* pOwner)
+		{ PUSH_VAR32(pOwner); PUSH_VAR32(cell); SET_REG32(EDX, deferment); SET_REG32(ECX, duration);
 			CALL(0x539EB0); }
 
 	static void RequestStop()
@@ -163,10 +163,10 @@ public:
 	static bool HasDeferment()
 		{ CALL(0x53A0E0); }
 
-	static void Strike(CellStruct Coords)
-		{ PUSH_VAR32(Coords); CALL(0x53A140); }
+	static void Strike(CellStruct cell)
+		{ PUSH_VAR32(cell); CALL(0x53A140); }
 
-	static void Strike2(CoordStruct Coords)
+	static void Strike2(CoordStruct coords)
 		{ JMP_STD(0x53A300); }
 
 	static void PrintMessage()
@@ -181,8 +181,8 @@ public:
 	static HouseClass* &Owner;
 	static AnimClass* &Anim;
 
-	static void Start(HouseClass *pOwner, CellStruct Coords)
-		{ PUSH_VAR32(Coords); SET_REG32(ECX, pOwner);
+	static void Start(HouseClass* pOwner, CellStruct coords)
+		{ PUSH_VAR32(coords); SET_REG32(ECX, pOwner);
 			CALL(0x53AE50); }
 
 	static void Update()
