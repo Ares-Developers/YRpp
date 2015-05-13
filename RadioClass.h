f5fbc09a1cb793532770b0c2b9e6c6bd54b39ef9
AlexB <Alexander-B@gmx.de>
2015-05-13 13:47:59 +0200

    Added remaining RadioClass methods
    GetNthLink's parameter now defaults to 0, so no GetFirstLink function is necessary.

--- RadioClass.h
+++ RadioClass.h
@@ -27,9 +27,34 @@ public:
 	virtual RadioCommand SendCommandWithData(RadioCommand command, AbstractClass* &pInOut, TechnoClass* pRecipient) RT(RadioCommand);
 	virtual void SendToEachLink(RadioCommand command) RX;
 
-	TechnoClass* GetNthLink(int idx) const {
+	// get specific link
+	TechnoClass* const& GetNthLink(int idx = 0) const {
 		return this->RadioLinks[idx];
 	}
+
+	// whether any link is pLink
+	bool ContainsLink(TechnoClass const* pLink) const
+		{ JMP_THIS(0x65AD50); }
+
+	// note: null pointers will always return -1
+	int FindLinkIndex(TechnoClass const* pLink) const
+		{ JMP_THIS(0x65AD90); }
+
+	// iow: not full
+	bool HasFreeLink() const
+		{ JMP_THIS(0x65ADC0); }
+
+	// iow: not full; consider pIgnore's link empty
+	bool HasFreeLink(TechnoClass const* pIgnore) const
+		{ JMP_THIS(0x65ADF0); }
+
+	// iow. at least one link used
+	bool HasAnyLink() const
+		{ JMP_THIS(0x65AE30); }
+
+	// resizes the vector and nulls the new elements
+	void SetLinkCount(int count)
+		{ JMP_THIS(0x65AE60); }
 
 	//Constructor
 	RadioClass() : MissionClass(false)
