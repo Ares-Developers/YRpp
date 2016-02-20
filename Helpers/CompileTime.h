a217423a51f9e7ea76b9a0c87da36f50a81df066
AlexB <Alexander-B@gmx.de>
2016-02-20 05:31:02 +0100

    Added a compile time reference helper struct, which can also imitate arrays.
    Because the dot operator can't be overloaded, reference repurposes the operator ->. If T is a pointer type, the referenced pointer will be dereferenced, if T is not a pointer type, operator -> will point to the referenced instance of T.

--- Helpers/CompileTime.h
+++ Helpers/CompileTime.h
@@ -31,3 +31,113 @@ public:
 		return *get();
 	}
 };
+
+// defines a compile time reference to a known memory address
+template <typename T, unsigned int Address, size_t Count = 0>
+struct reference {
+	using value_type = T[Count];
+
+	static const auto Size = Count;
+
+	constexpr reference() noexcept = default;
+private:
+	// mere presence "fixes" C2100: illegal indirection
+	constexpr reference(noinit_t) noexcept {}
+public:
+
+	value_type& get() const noexcept {
+		// fixes" C2101: '&' on constant
+		static auto const address = Address;
+		return *reinterpret_cast<value_type*>(address);
+	}
+
+	operator value_type&() const noexcept {
+		return get();
+	}
+
+	value_type& operator()() const noexcept {
+		return get();
+	}
+
+	decltype(auto) operator&() const noexcept {
+		return &get();
+	}
+
+	decltype(auto) operator*() const noexcept {
+		return *get();
+	}
+
+	T& operator[](int index) const noexcept {
+		return get()[index];
+	}
+
+	value_type& data() const noexcept {
+		return get();
+	}
+
+	size_t size() const noexcept {
+		return Size;
+	}
+
+	T* begin() const noexcept {
+		return data();
+	}
+
+	T* end() const noexcept {
+		return begin() + Size;
+	}
+};
+
+// specializations for non-array references
+template <typename T, unsigned int Address>
+struct reference<T, Address, 0> {
+	using value_type = T;
+
+	constexpr reference() noexcept = default;
+private:
+	// mere presence "fixes" C2100: illegal indirection
+	constexpr reference(noinit_t) noexcept {}
+public:
+
+	value_type& get() const noexcept {
+		return *reinterpret_cast<value_type*>(Address);
+	}
+
+	template <typename T2, typename = std::enable_if_t<std::is_assignable<T&, T2>::value>>
+	T& operator=(T2&& rhs) const {
+		return get() = std::forward<T2>(rhs);
+	}
+
+	operator T&() const noexcept {
+		return get();
+	}
+
+	T& operator()() const noexcept {
+		return get();
+	}
+
+	decltype(auto) operator&() const noexcept {
+		return &get();
+	}
+
+	decltype(auto) operator->() const noexcept {
+		return arrow(std::is_pointer<T>::type());
+	}
+
+	decltype(auto) operator*() const noexcept {
+		return *get();
+	}
+
+	decltype(auto) operator[](int index) const noexcept {
+		return get()[index];
+	}
+
+private:
+	auto arrow(std::false_type) const noexcept {
+		return &get();
+	}
+
+	auto arrow(std::true_type) const noexcept {
+		return get();
+	}
+};
